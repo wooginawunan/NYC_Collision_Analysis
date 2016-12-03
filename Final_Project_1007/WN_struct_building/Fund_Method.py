0644faf162d50dbc79fa50969336865008bd9401
Shucheng Yan <sy1253@nyu.edu>
2016-12-03 18:49:34 -0500

    Fund_Method Only num Untested

--- Final_Project_1007/WN_struct_building/Fund_Method.py
+++ Final_Project_1007/WN_struct_building/Fund_Method.py
@@ -1,126 +1,114 @@
 
 def TotalAccident(self, level, name, timeStart, timeEnd):
 	num = 0
-	info = {}
 	# if(level == "City"):
 
 	if(level == "Borough"):
 		for n in name:
 			for p in self.data.Borough_Dict[n].precinctList:
-				num = num + TotalAccident(self, "Precinct", p.ID).num
-				info.append(TotalAccident(self, "Precinct", p.ID).info)
+				num = num + TotalAccident(self, "Precinct", p.ID)
 	if(level == "Precinct"):
 		for n in name:
 			for b in ['bk', 'bx','mn','qn','si']:
 				if (self.data.Borough_Dict[b].precinctList[name] != null) :
-					for Year, Month, Intersection in self.data.Borough_Dict[b].precinctList[name]:
+					for Year, Month, collisions_I in self.data.Borough_Dict[b].precinctList[name].Collisions_intersection:
 						num = num + 1
-						info.append({	'city': NYC.Borough_Dict[n],
-										'Year': Year, 
-										'Month': Month,
-										'Intersection': {
-											'collisions': Intersection.collisions,
-											'factors': Intersection.factors},
-											'HighTunBri':{
-												'collisions': Intersection.HighTunBri.collisions,
-												'factors': Intersection.HighTunBri.factors
-											}
-										})	
+					for Year, Month, collisions_H in self.data.Borough_Dict[b].precinctList[name].Collisions_HighTunBri:
+						num = num + 1
 					break
 
 
 	if(level == "Road"):
 		for n in name:
-			for Year, Month, Intersection in self.data.Road_Dict[n]:
+			for Year, Month, Collisions in self.data.Road_Dict[n]:
 				num = num + 1
-				info.append({	'city': NYC.Road_Dict[area],
-								'Year': Year, 
-								'Month': Month,
-								'Intersection': {
-									'collisions': Intersection.collisions,
-									'factors': Intersection.factors},
-									'HighTunBri':{
-										'collisions': Intersection.HighTunBri.collisions,
-										'factors': Intersection.HighTunBri.factors
-									}
-								})
 
 	if(level == "Highway"):
 		for n in name:
-			for Year, Month, Intersection in self.data.Highway_Dict[n]:
+			for Year, Month, Collisions in self.data.Highway_Dict[n]:
 				num = num + 1
-				info.append({	'city': NYC.Highway_Dict[area],
-								'Year': Year, 
-								'Month': Month,
-								'Intersection': {
-									'collisions': Intersection.collisions,
-									'factors': Intersection.factors},
-									'HighTunBri':{
-										'collisions': Intersection.HighTunBri.collisions,
-										'factors': Intersection.HighTunBri.factors
-									}
-								})
 	if(level == "Bridge"):
 		for n in name:
-			for Year, Month, Intersection in self.data.Bridge_Dict[n]:
+			for Year, Month, Collisions in self.data.Bridge_Dict[n]:
 				num = num + 1
-				info.append({	'city': NYC.Bridge_Dict[area],
-								'Year': Year, 
-								'Month': Month,
-								'Intersection': {
-									'collisions': Intersection.collisions,
-									'factors': Intersection.factors},
-									'HighTunBri':{
-										'collisions': Intersection.HighTunBri.collisions,
-										'factors': Intersection.HighTunBri.factors
-									}
-								})
 	if(level == "Tunnel"):
 		for n in name:
-			for Year, Month, Intersection in self.data.Tridge_Dict[n]:
+			for Year, Month, Collisions in self.data.Tunnel_Dict[n]:
 				num = num + 1
-				info.append({	'city': NYC.Tridge_Dict[area],
-								'Year': Year, 
-								'Month': Month,
-								'Intersection': {
-									'collisions': Intersection.collisions,
-									'factors': Intersection.factors},
-									'HighTunBri':{
-										'collisions': Intersection.HighTunBri.collisions,
-										'factors': Intersection.HighTunBri.factors
-									}
-								})
+	return num
 
-	return {num, info}
-
-def TotalInjure(self, level, name, timeStart, timeEnd):
+def TotalInjury(self, level, name, timeStart, timeEnd):
 	num = 0
-	info = {}
+	# if(level == "City"):
+
 	if(level == "Borough"):
 		for n in name:
-			for Year, Month, Intersection in self.data.Borough_Dict[name]:
-				num = num + self.data.Borough_Dict[name].Intersection.collisions.PersonsInjured
-				info.append({	'city': NYC.Borough_Dict[name],
-								'Year': Year, 
-								'Month': Month,
-								'Intersection': {
-									'collisions': Intersection.collisions,
-									'factors': Intersection.factors},
-									'HighTunBri':{
-										'collisions': Intersection.HighTunBri.collisions,
-										'factors': Intersection.HighTunBri.factors
-									}
-								})
+			for p in self.data.Borough_Dict[n].precinctList:
+				num = num + TotalAccident(self, "Precinct", p.ID)
+	if(level == "Precinct"):
+		for n in name:
+			for b in ['bk', 'bx','mn','qn','si']:
+				if (self.data.Borough_Dict[b].precinctList[name] != null) :
+					for Year, Month, collisions_I in self.data.Borough_Dict[b].precinctList[name].Collisions_intersection:
+						num = num + collisions_I.PersonsInjured
+					for Year, Month, collisions_H in self.data.Borough_Dict[b].precinctList[name].Collisions_HighTunBri:
+						num = num + Collisions_H.PersonsInjured
+					break
 
 
-	return {num, info}
+	if(level == "Road"):
+		for n in name:
+			for Year, Month, Collisions in self.data.Road_Dict[n]:
+				num = num + Collisions.PersonsInjured
 
-def TotalKill(path):
+	if(level == "Highway"):
+		for n in name:
+			for Year, Month, Collisions in self.data.Highway_Dict[n]:
+				num = num + Collisions.PersonsInjured
+	if(level == "Bridge"):
+		for n in name:
+			for Year, Month, Collisions in self.data.Bridge_Dict[n]:
+				num = num + Collisions.PersonsInjured
+	if(level == "Tunnel"):
+		for n in name:
+			for Year, Month, Collisions in self.data.Tunnel_Dict[n]:
+				num = num + Collisions.PersonsInjured
+	return num
 
-	return count
+def TotalKilled(self, level, name, timeStart, timeEnd):
+	num = 0
+	# if(level == "City"):
 
-def TotalInjure(path):
+	if(level == "Borough"):
+		for n in name:
+			for p in self.data.Borough_Dict[n].precinctList:
+				num = num + TotalAccident(self, "Precinct", p.ID)
+	if(level == "Precinct"):
+		for n in name:
+			for b in ['bk', 'bx','mn','qn','si']:
+				if (self.data.Borough_Dict[b].precinctList[name] != null) :
+					for Year, Month, collisions_I in self.data.Borough_Dict[b].precinctList[name].Collisions_intersection:
+						num = num + collisions_I.PersonsKilled
+					for Year, Month, collisions_H in self.data.Borough_Dict[b].precinctList[name].Collisions_HighTunBri:
+						num = num + Collisions_H.PersonsKilled
+					break
 
 
-	return count
+	if(level == "Road"):
+		for n in name:
+			for Year, Month, Collisions in self.data.Road_Dict[n]:
+				num = num + Collisions.PersonsKilled
 
+	if(level == "Highway"):
+		for n in name:
+			for Year, Month, Collisions in self.data.Highway_Dict[n]:
+				num = num + Collisions.PersonsKilled
+	if(level == "Bridge"):
+		for n in name:
+			for Year, Month, Collisions in self.data.Bridge_Dict[n]:
+				num = num + Collisions.PersonsKilled
+	if(level == "Tunnel"):
+		for n in name:
+			for Year, Month, Collisions in self.data.Tunnel_Dict[n]:
+				num = num + Collisions.PersonsKilled
+	return num
